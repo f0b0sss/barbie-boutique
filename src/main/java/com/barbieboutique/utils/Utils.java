c3ae712506a4f483fbed26d4c6090ee38977b1b6
dmytroimin <dmitriy3imin@gmail.com>
2023-03-10 09:34:49 +0200

    add README.md file

--- src/main/java/com/barbieboutique/utils/Utils.java
+++ src/main/java/com/barbieboutique/utils/Utils.java
@@ -48,23 +48,12 @@ public class Utils {
     private Image convertMultipartToImage(MultipartFile file) {
         Image image = null;
 
-//                try {
-//            image = Image.builder()
-//                    .name(file.getName())
-//                    .originalFileName(file.getOriginalFilename())
-//                    .contentType(file.getContentType())
-//                    .size(file.getSize())
-//                    .image(Base64.getEncoder().encodeToString(file.getBytes()))
-////                    .bytes(file.getBytes())
-//                    .build();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        String filename = UUID.randomUUID().toString();
 
         try {
             image = Image.builder()
                     .name(file.getName())
-                    .originalFileName(file.getOriginalFilename())
+                    .originalFileName(filename)
                     .contentType(file.getContentType())
                     .size(file.getSize())
                     .bytes(file.getBytes())
